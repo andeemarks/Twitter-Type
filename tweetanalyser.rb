af220cd97476afbf41c1edc4a0c49d3677a9857e
Andy Marks <andy@corvine.org>
2010-01-15 08:02:09 +1100

    Added more type tests around tweet analyser args.  All green.

--- tweetanalyser.rb
+++ tweetanalyser.rb
@@ -5,13 +5,14 @@ class TweetAnalyser
   
   def initialize(profile)
     raise ArgumentError if profile.class != TweeterProfile
+    
     @profile = profile
   end
   
   def analyse(tweets)
     raise ArgumentError.new("No tweets to analyse") if tweets.nil?
     raise ArgumentError.new("Tweets are not in array") if tweets.class != Array
-    raise ArgumentError.new("Array does not contain tweets") if tweets.count > 0 and tweets.first.class != Hashie::Mash
+#    raise ArgumentError.new("Array does not contain tweets") if tweets.count > 0 and "Hashie::Mash".eql(tweets.first.inspect)
 
     tweets.each do |tweet|
       analyse_tweet(tweet)
@@ -21,11 +22,15 @@ class TweetAnalyser
   end
   
   def analyse_tweet(tweet) 
-    @profile.tweet_count = @profile.tweet_count + 1
-    @profile.reply_count = @profile.reply_count + 1 if tweet.to_user != nil 
-    @profile.retweet_count = @profile.retweet_count + 1 if tweet.text.start_with?('RT')
-    @profile.link_count = @profile.link_count + 1 if tweet.text.index('http://') != nil
-    @profile.last_tweet_id = tweet.id if tweet.id > @profile.last_tweet_id
+    begin
+      @profile.tweet_count = @profile.tweet_count + 1
+      @profile.reply_count = @profile.reply_count + 1 if tweet.to_user != nil 
+      @profile.retweet_count = @profile.retweet_count + 1 if tweet.text.start_with?('RT')
+      @profile.link_count = @profile.link_count + 1 if tweet.text.index('http://') != nil
+      @profile.last_tweet_id = tweet.id if tweet.id > @profile.last_tweet_id
+    rescue NoMethodError => root
+      raise ArgumentError.new("Missing method responses in tweet structure:" + root.to_s)
+    end
   end
   
 end
--- tweetanalyser_spec.rb
+++ tweetanalyser_spec.rb
@@ -1,7 +1,12 @@
 require 'tweetanalyser'
 
 describe TweetAnalyser do
-  it "requires a profile on construction" do
+  before(:each) do
+    @empty_profile = TweeterProfile.new(nil)
+    @basic_analyser = TweetAnalyser.new(@empty_profile)
+  end
+  
+  it "will fail construction without a profile" do
     lambda {TweetAnalyser.new}.should raise_error(ArgumentError)
     lambda {TweetAnalyser.new(nil)}.should raise_error(ArgumentError)
     lambda {TweetAnalyser.new(1)}.should raise_error(ArgumentError)
@@ -10,20 +15,35 @@ describe TweetAnalyser do
     TweetAnalyser.new(TweeterProfile.new(nil))
   end
   
-  it "requires an array of tweets to analyse" do
-    analyser = TweetAnalyser.new(TweeterProfile.new(nil))
-    lambda {analyser.analyse()}.should raise_error(ArgumentError)
-    lambda {analyser.analyse(nil)}.should raise_error(ArgumentError)
-    lambda {analyser.analyse("tweets")}.should raise_error(ArgumentError)
-    lambda {analyser.analyse([1, 2, 3])}.should raise_error(ArgumentError)
+  it "will fail analysis without an array of tweets" do
+    lambda {@basic_analyser.analyse()}.should raise_error(ArgumentError)
+    lambda {@basic_analyser.analyse(nil)}.should raise_error(ArgumentError)
+    lambda {@basic_analyser.analyse("tweets")}.should raise_error(ArgumentError)
+    lambda {@basic_analyser.analyse([1, 2, 3])}.should raise_error(ArgumentError)
     
-    analyser.analyse(Array.new(1, Hashie::Mash))
+    @basic_analyser.analyse(Array.new())
   end
   
   it "will return the original profile if no tweets are supplied" do
-    original_profile = TweeterProfile.new(nil)
-    analyser = TweetAnalyser.new(original_profile)
-    analyser.analyse(Array.new()).should eql(original_profile)
+    @basic_analyser.analyse(Array.new()).should eql(@empty_profile)
+  end
+  
+  it "will need a validly set of fields in the tweet" do
+    lambda {@basic_analyser.analyse(Array.new(1, "tweet"))}.should raise_error(ArgumentError)
+
+    tweet_with_valid_fields = mock()
+    tweet_with_valid_fields.stub!(:to_user).and_return("user")
+    tweet_with_valid_fields.stub!(:text).and_return("text")
+    tweet_with_valid_fields.stub!(:id).and_return(12345678)
+    
+    @basic_analyser.analyse(Array.new(1, tweet_with_valid_fields))
+  end
+  
+  it "will need the tweet text to be a string" do
+    tweet_with_non_string_text = mock()
+    tweet_with_non_string_text.stub!(:to_user).and_return("user")
+    tweet_with_non_string_text.stub!(:text).and_return(1)
+    
+    lambda {@basic_analyser.analyse(Array.new(1, tweet_with_non_string_text))}.should raise_error(ArgumentError)
   end
 end
-
