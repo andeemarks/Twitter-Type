ecdb77aee224963dd13ce28baaddbaf311ff1cba
Andy Marks <andy@corvine.org>
2010-01-23 21:23:23 +1100

    Refactored test setup into common method.  Would be nice to only need to specify the values I wanted to change though.

--- test/typeinferrer_spec.rb
+++ test/typeinferrer_spec.rb
@@ -3,40 +3,36 @@ require "src/typeinferrer"
 require "src/twittertype"
 
 describe TypeInferrer do
-  before(:each) do
+  before(:all) do
     @mock_profile = mock()
+    @cut = TypeInferrer.new
   end
 
   it "should infer a type of retweeter from a profile with predominantly retweets" do
-    @mock_profile.stub!(:retweet_count).and_return(1)
-    @mock_profile.stub!(:link_count).and_return(0)
-    @mock_profile.stub!(:reply_count).and_return(0)
-    @mock_profile.stub!(:tweet_count).and_return(1)
-    TypeInferrer.new.infer(@mock_profile).should == TwitterType::RETWEETER
+    setup_profile({:retweet_count => 1, :link_count => 0, :reply_count => 0, :tweet_count => 1})
+    @cut.infer(@mock_profile).should == TwitterType::RETWEETER
   end
 
   it "should infer a type of linker from a profile with predominantly links" do
-    @mock_profile.stub!(:link_count).and_return(1)
-    @mock_profile.stub!(:retweet_count).and_return(0)
-    @mock_profile.stub!(:reply_count).and_return(0)
-    @mock_profile.stub!(:tweet_count).and_return(1)
-    TypeInferrer.new.infer(@mock_profile).should == TwitterType::LINKER
+    setup_profile({:retweet_count => 0, :link_count => 1, :reply_count => 0, :tweet_count => 1})
+    @cut.infer(@mock_profile).should == TwitterType::LINKER
   end
 
   it "should infer a type of chatter from a profile with predominantly replies" do
-    @mock_profile.stub!(:link_count).and_return(0)
-    @mock_profile.stub!(:retweet_count).and_return(0)
-    @mock_profile.stub!(:reply_count).and_return(1)
-    @mock_profile.stub!(:tweet_count).and_return(1)
-    TypeInferrer.new.infer(@mock_profile).should == TwitterType::CHATTER
+    setup_profile({:retweet_count => 0, :link_count => 0, :reply_count => 1, :tweet_count => 1})
+    @cut.infer(@mock_profile).should == TwitterType::CHATTER
   end
 
   it "should infer a type of originator from a profile with predominantly original tweets" do
-    @mock_profile.stub!(:link_count).and_return(1)
-    @mock_profile.stub!(:retweet_count).and_return(1)
-    @mock_profile.stub!(:reply_count).and_return(1)
-    @mock_profile.stub!(:tweet_count).and_return(10)
-    TypeInferrer.new.infer(@mock_profile).should == TwitterType::ORIGINATOR
+    setup_profile({:retweet_count => 1, :link_count => 1, :reply_count => 1, :tweet_count => 10})
+    @cut.infer(@mock_profile).should == TwitterType::ORIGINATOR
   end
 
+
+  def setup_profile(return_values)
+    @mock_profile.stub!(:retweet_count).and_return(return_values[:retweet_count])
+    @mock_profile.stub!(:link_count).and_return(return_values[:link_count])
+    @mock_profile.stub!(:reply_count).and_return(return_values[:reply_count])
+    @mock_profile.stub!(:tweet_count).and_return(return_values[:tweet_count])
+  end
 end
